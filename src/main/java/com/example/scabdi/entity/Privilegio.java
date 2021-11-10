30c98c8f8c6475a44af36148a4489f40542ade13
b3nj4hb <benjalive12@gmail.com>
2021-11-09 19:26:29 -0500

    Algunas relaciones

--- src/main/java/com/example/scabdi/entity/Privilegio.java
+++ src/main/java/com/example/scabdi/entity/Privilegio.java
@@ -1,5 +1,50 @@
 package com.example.scabdi.entity;
 
-public class Privilegio {
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.OneToMany;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Setter
+@Getter
+@Table(name = "tsg_privilegio")
+
+public class Privilegio implements Serializable {
+
+	private static final long serialVersionUID = 3672608267631946326L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_privilegio")
+	private int id;
+
+	private String no_privilegio;
+	
+	//Relaciones
+	
+	@OneToMany(cascade = CascadeType.MERGE, fetch = FetchType.LAZY, mappedBy = "id_privilegio")
+	@JsonIgnore
+	private List<RolPrivilegio> rolPrivilegio = new ArrayList<>();
 }
--- src/main/java/com/example/scabdi/entity/Rol.java
+++ src/main/java/com/example/scabdi/entity/Rol.java
@@ -1,5 +1,54 @@
 package com.example.scabdi.entity;
 
-public class Rol {
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.OneToMany;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Setter
+@Getter
+@Table(name = "tsg_rol")
+
+public class Rol implements Serializable {
+
+	private static final long serialVersionUID = 6910084575999727947L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_rol")
+	private int id;
+
+	private String no_rol;
+
+	//Relaciones
+	
+	@OneToMany(cascade = CascadeType.MERGE, fetch = FetchType.LAZY, mappedBy = "id_rol")
+	@JsonIgnore
+	private List<RolPrivilegio> rolPrivilegio = new ArrayList<>();
+	
+	@OneToMany(cascade = CascadeType.MERGE, fetch = FetchType.LAZY, mappedBy = "id_rol")
+	@JsonIgnore
+	private List<UsuarioRol> usuarioRol = new ArrayList<>();
 }
