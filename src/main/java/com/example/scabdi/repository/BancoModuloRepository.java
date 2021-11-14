90d4c040c716874ccf2ddc89504d28f14b11750c
Digi85 <chrisdigi85@gmail.com>
2021-11-14 18:20:05 -0500

    Dg repositorio listo

--- src/main/java/com/example/scabdi/repository/BancoModuloRepository.java
+++ src/main/java/com/example/scabdi/repository/BancoModuloRepository.java
@@ -1,11 +1,36 @@
 package com.example.scabdi.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.scabdi.entity.BancoModulo;
+import com.example.scabdi.entity.Banco_Modulo;
 
 @Repository
 public interface BancoModuloRepository extends JpaRepository<BancoModulo, Integer>{
+	// LISTAR
+		@Query(value = "{call SP_LISTA_TBL_BANCO_MODULO()}", nativeQuery = true)
+		List<Banco_Modulo> listabanco_modulos();
+
+		// BUSCAR
+		@Query(value = "{call SP_BUSCAR_TBL_BANCO_MODULO(:p_nID_BANCO_MODULO)}", nativeQuery = true)
+		Optional<Banco_Modulo> buscar(@Param("p_nID_BANCO_MODULO") int p_nID_BANCO_MODULO);
+
+		// INSERTAR
+		@Query(value = "{call SP_INS_TBL_BANCO_MODULO(:p_nID_BANCO_COMUNAL,:p_nID_MODULO, @p_nID_BANCO_MODULO)}", nativeQuery = true)
+		String insertar(@Param("p_nID_BANCO_COMUNAL") int p_nID_BANCO_COMUNAL, @Param("p_nID_MODULO") int p_nID_MODULO);
+
+		// ELIMINAR
+		@Query(value = "{call SP_DEL_TBL_BANCO_MODULO(:p_nID_BANCO_MODULO)}", nativeQuery = true)
+		String eliminar(@Param("p_nID_BANCO_MODULO") int p_nID_BANCO_MODULO);
+
+		// ACTULALIZAR
+		@Query(value = "{CALL SP_UPD_TBL_BANCO_MODULO(:p_nID_BANCO_MODULO, :p_nID_BANCO_COMUNAL,:p_nID_MODULO, @P_OK)}", nativeQuery = true)
+		String actualizar(@Param("p_nID_BANCO_MODULO") int p_nID_BANCO_MODULO, @Param("p_nID_BANCO_COMUNAL") int p_nID_BANCO_COMUNAL , @Param("p_nID_MODULO") int p_nID_MODULO );
 
 }
--- src/main/java/com/example/scabdi/repository/ModuloRepository.java
+++ src/main/java/com/example/scabdi/repository/ModuloRepository.java
@@ -1,11 +1,30 @@
 package com.example.scabdi.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.scabdi.entity.Modulo;
 
 @Repository
 public interface ModuloRepository extends JpaRepository<Modulo, Integer>{
-
+	    //LISTAR
+		@Query(value = "{call SP_LISTA_TBL_MODULO()}", nativeQuery = true) 
+		List<Modulo> listamodulos();
+		//BUSCAR
+		@Query(value = "{call SP_BUSCAR_TBL_MODULO(:p_nID_MODULO)}", nativeQuery = true)
+		Optional<Modulo> buscar(@Param("p_nID_MODULO") int p_nID_MODULO);
+		// INSERTAR
+	    @Query(value = "{call SP_INS_TBL_MODULO(:p_nID_AREA ,:p_vNO_MODULO,:p_vDE_MODULO,:p_nCA_TOTAL_RECURSOS ,@p_nID_MODULO)}", nativeQuery = true)
+	    String insertar(@Param("p_nID_AREA") int p_nID_AREA, @Param("p_vNO_MODULO") String p_vNO_MODULO, @Param("p_vDE_MODULO") String p_vDE_MODULO, @Param("p_nCA_TOTAL_RECURSOS") int p_nCA_TOTAL_RECURSOS);
+	    // ELIMINAR
+	 	@Query(value = "{call SP_DEL_TBL_MODULO(:p_nID_MODULO)}", nativeQuery = true)
+	 	String eliminar(@Param("p_nID_MODULO") int p_nID_MODULO);
+	    // ACTULALIZAR
+	 	@Query(value = "{CALL SP_UPD_TBL_MODULO(:p_nID_MODULO,:p_nID_AREA,:p_vNO_MODULO,:p_vDE_MODULO,:p_nCA_TOTAL_RECURSOS, @P_OK)}", nativeQuery = true)
+	 	String actualizar(@Param("p_nID_MODULO") int p_nID_MODULO, @Param("p_nID_AREA") int p_nID_AREA,@Param("p_vNO_MODULO") String p_vNO_MODULO, @Param("p_vDE_MODULO") String p_vDE_MODULO, @Param("p_nCA_TOTAL_RECURSOS") int p_nCA_TOTAL_RECURSOS);
 }
