b82f8ec282c9365e0df487fec7d6ad962012da2c
b3nj4hb <benjalive12@gmail.com>
2021-12-13 23:37:03 -0500

    sin local host

--- src/main/java/com/example/scabdi/config/ResourceServerConfig.java
+++ src/main/java/com/example/scabdi/config/ResourceServerConfig.java
@@ -32,7 +32,9 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
 		CorsConfiguration config = new CorsConfiguration();
 		config.setAllowedOrigins(Arrays.asList("https://scabdi.herokuapp.com"));
 		config.setAllowedOrigins(Arrays.asList("https://scabdi-168f3.web.app"));
+		/*
 		config.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+		*/
 		config.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE","OPTIONS"));
 		config.setAllowCredentials(true);
 		config.setAllowedHeaders(Arrays.asList("Content-Type","Authorization"));
